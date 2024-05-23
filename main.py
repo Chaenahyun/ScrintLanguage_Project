eb1c623b5b9d6e0647739d6805e9c66fa333bdde
Chaenahyun <144212525+Chaenahyun@users.noreply.github.com>
2024-05-24 05:04:01 +0900

    Add Search Button

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from tkinter import *
-from tkinter.font import *
-from search import *
+from tkinter import Tk, Label, Frame, Button
+from tkinter.font import Font
+from search import SearchFrame
 
 frameHeight = 580
 WIDTH = 960
@@ -10,29 +10,27 @@ class MainGUI:
     def __init__(self):
         self.window = Tk()
         self.window.title("학원 교습소 검색 프로그램")
-        self.window.geometry(str(WIDTH) + 'x' + str(HEIGHT))
+        self.window.geometry(f"{WIDTH}x{HEIGHT}")
         self.window.resizable(False, False)
 
-        title_font = Font(family='italic', size=40, slant='italic')
+        APP = Font(family='bauhaus 93', size=40, slant='italic')
+        nameTag = Label(self.window, text='APP', font=APP, width=4)
+        nameTag.grid(row=0, column=0, sticky='w')
 
-        nameTag = Label(self.window, text='APP', font=title_font)
-        nameTag.grid(row=0, column=0)
-
-        self.searchFrame = Frame(self.window, width=int(WIDTH / 2 - 30), height=frameHeight, relief=GROOVE)
-        self.searchFrame.grid(row=1, column=0, sticky="nsew")
+        self.searchFrame = Frame(self.window, width=WIDTH / 2 - 30, height=frameHeight)
+        self.searchFrame.grid(row=1, column=0)
         SearchFrame(self.searchFrame)
 
-        self.mapFrame = Frame(self.window, width=int(WIDTH / 2 - 60), height=frameHeight)
-        self.mapFrame.grid(row=1, column=1, sticky="nsew")
+        self.buttonFont = Font(family='italic', weight='bold', slant='roman', size=30)
+        self.button = Button(self.window, text='검색', width=4, height=1, relief='raised', bd=4, font=self.buttonFont,
+                             command=SearchFrame.search)
+        self.button.place(x=300, y=100)
+
+        self.mapFrame = Frame(self.window, width=WIDTH / 2 - 60, height=frameHeight)
+        self.mapFrame.grid(row=1, column=1)
 
         self.funcFrame = Frame(self.window, width=90, height=frameHeight)
-        self.funcFrame.grid(row=1, column=2, sticky="nsew")
-
-        self.leftFrame = Frame(self.window, width=540, height=700)
-        self.leftFrame.grid(row=2, column=0, columnspan=3, sticky="nsew")
-
-        self.rightFrame = Frame(self.window, width=540, height=700)
-        self.rightFrame.grid(row=3, column=0, columnspan=3, sticky="nsew")
+        self.funcFrame.grid(row=1, column=2)
 
         self.window.mainloop()
 
