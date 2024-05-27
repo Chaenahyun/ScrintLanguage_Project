9c3a4cabbaabb79b52c26c07dfb71ec6a6092f5f
Chaenahyun <144212525+Chaenahyun@users.noreply.github.com>
2024-05-27 18:49:28 +0900

    Add Map Button

--- map.py
+++ map.py
@@ -1,12 +1,19 @@
-
 from xml import *
 from tkinter import *
 from tkinter.font import *
 import tkinter.ttk
-
-
 class MapFrame:
     def __init__(self, frame):
         self.Labelfont = Font(family='italic', weight='bold', slant='roman', size=20)
         mapLabel = Label(frame, text='지도', font=self.Labelfont)
         mapLabel.place(x=15, y=25)
+
+        #지도 연결 버튼
+        self.initButton(frame)
+
+    def initButton(self, frame):
+        self.mapButton = Button(frame, text='지도 보기', width=6, height=1, bd=4, relief='raised', font=self.Labelfont,
+                                command=self.connectMap).place(x=15, y=100)
+
+    def connectMap(self):
+        pass
--- search.py
+++ search.py
@@ -2,42 +2,33 @@ from xml import *
 from tkinter import *
 from tkinter.font import *
 import tkinter.ttk
-
 class SearchFrame:
     def __init__(self, frame):
-        self.font = Font(family='italic', weight='bold', slant='roman')
-        cityLabel = Label(frame, text='시/군', font=self.font)
-        cityLabel.place(x=30, y=25)
         self.Combofont = Font(family='italic', weight='bold', slant='roman', size=14)
         self.Labelfont = Font(family='italic', weight='bold', slant='roman', size=20)
-
+        #시/군 라벨 생성, 위치 지정
         cityLabel = Label(frame, text='시/군', font=self.Labelfont)
         cityLabel.place(x=15, y=25)
         values = [str(i) for i in range(10)]
-        cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5)
         cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5, font=self.Combofont)
         cityCombo.configure(state='readonly')
-        cityCombo.place(x=100, y=30)
         cityCombo.place(x=90, y=30)
         frame.option_add('*TCombobox*Listbox.font', self.Combofont)
-        cityCombo.set('목록 선택')
-
-        jobLabel = Label(frame, text='업종', font=self.font)
-        jobLabel.place(x=30, y=80)
+        cityCombo.set('목록')
+        #업종 라벨 생성, 위치 지정
         jobLabel = Label(frame, text='업종', font=self.Labelfont)
         jobLabel.place(x=15, y=80)
         values = [str(i) for i in range(10)]
-        cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5)
         cityCombo = tkinter.ttk.Combobox(frame, values=values, height=5, font=self.Combofont)
         cityCombo.configure(state='readonly')
-        cityCombo.place(x=100, y=85)
         cityCombo.place(x=90, y=85)
         frame.option_add('*TCombobox*Listbox.font', self.Combofont)
-        cityCombo.set('목록 선택')
-
-
-        resultLabel = Label(frame, text='학원 및 교습소 명', font=self.Labelfont)
-        resultLabel.place(x=15, y=160)
+        cityCombo.set('목록')
+        #검색 버튼 생성, 위치 지정
+        Button(frame, text='검색', width=4, height=1, relief='raised', bd=4, font=self.Labelfont,
+                             command=self.search).place(x=350, y=50)
+        #라벨 생성, 위치 지정
+        Label(frame, text='학원 및 교습소 명', font=self.Labelfont).place(x=15, y=160)
 
     def search(self):
         pass
