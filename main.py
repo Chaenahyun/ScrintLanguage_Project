cb7d4eacc6cd08a2655a2599d8cb52a8033fe7fa
Chaenahyun <144212525+Chaenahyun@users.noreply.github.com>
2024-05-28 20:30:05 +0900

    Add Bookmark

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ frameHeight = 580
 WIDTH = 960
 HEIGHT = 640
 
-
 class MainGUI:
     def __init__(self):
         self.window = Tk()
@@ -17,32 +16,22 @@ class MainGUI:
         self.window.resizable(False, False)
 
         APP = Font(family='bauhaus 93', size=40, slant='italic')
-        nameTag = Label(self.window, text='APP', font=APP, width=4)
-        nameTag.grid(row=0, column=0, sticky='w')
+        nameTag = Label(self.window, text='STUDY WITH ME', font=APP, width=40, anchor='w', bg='light cyan')
+        nameTag.grid(row=0, column=0)
 
-        self.searchFrame = Frame(self.window, width=WIDTH, height=frameHeight, bg='light gray')
+        self.searchFrame = Frame(self.window, width=WIDTH, height=frameHeight, bg='light green')
         self.searchFrame.place(x=0, y=60)
         SearchFrame(self.searchFrame)
-        # self.buttonFont = Font(family='italic', weight='bold', slant='roman', size=20)
-        # self.button = Button(self.window, text='검색', width=4, height=1, relief='raised', bd=4, font=self.buttonFont,
-        #                      command=SearchFrame.search)
-        # self.button.place(x=350, y=100)
 
-        self.mapFrame = Frame(self.window, width=WIDTH / 2 - 60, height=frameHeight - 50, bg='magenta')
-        self.mapFrame.place(x=450, y=130)
-        # self.mapFrame = Frame(self.window, width=WIDTH / 2 - 60, height=frameHeight - 80, bg='magenta')
-        # self.mapFrame.place(x=450, y=130)
+        self.mapFrame = Frame(self.window, width=WIDTH / 2 - 60, height=80, bg='light green')
+        self.mapFrame.place(x=450, y=100)
         MapFrame(self.mapFrame)
 
-        # telegram., mail icon frame
-        self.funcFrame = Frame(self.window, width=70, height=frameHeight - 50, bg='cyan')
-        self.funcFrame.place(x=880, y=100)
+        #telegram., mail icon frame
+        self.funcFrame = Frame(self.window, width=95, height=frameHeight - 50, bg='light cyan')
+        self.funcFrame.place(x=855, y=100)
 
-        # self.resultFrame = Frame(self.window, width=WIDTH / 2 - 50, height=frameHeight / 2 + 60, bg='green')
-        # self.resultFrame.place(x=10, y=280)
 
         self.window.mainloop()
         pass
-
-
 MainGUI()
